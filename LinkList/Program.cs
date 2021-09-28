cbf2cfe87e0ea8038faf3b191a348be056235b9b
Melicha <melissa.chavez@tectijuana.edu.mx>
2021-09-28 07:55:32 -0700

    Listas doblemente enlazadas circular

--- LinkList/Program.cs
+++ LinkList/Program.cs
@@ -8,17 +8,19 @@ namespace LinkList
 {
 
     //Creamos una clase para el nodo
-    internal class LDENode
+    internal class LDCNode
     {
         //Creamos la estructura que va a contener los nodos
         //Dato del nodo entero
         internal int data;
 
         //Nodo siguiente=nulo 
-        internal LDENode next;
+        internal LDCNode next;
 
         //Nodo atras
-        internal LDENode back;
+        internal LDCNode back;
+
+
 
         //Declaramos metodo get(obtener) retorna datos, metodo set(colocar) datos=valor
         public int Data
@@ -28,39 +30,39 @@ namespace LinkList
         }
 
         //Declaramos metodo get(obtener) retorna nodo siguiente, metodo set(colocar) siguiente=valor 
-        public LDENode Next
+        public LDCNode Next
         {
             get { return next; }
             set { next = value; }
         }
         //Encapsula 
         //Declaramos metodo get(obtener) retorna nodo atras, metodo set(colocar) atras=valor
-        public LDENode Back
+        public LDCNode Back
         {
             get { return back; }
             set { back = value; }
         }
         //Se crea un constructor para indicarle el valor de las variables data = entero  y next= null
         //Constructor
-        public LDENode(int d)
+        public LDCNode(int d)
         {
             data = d;
             next = null;
         }
     }
-    //Creamos una clase para lista doblemente enlazada 
-    //Que significa que sea una Lista de Doblemente enlazadas?, 
-    //Significa que puede recorrer los valores desde el primer nodo hasta el ultimo y diseversa (doble) del ultimo hasta el primero
+    //Creamos una clase para lista doblemente enlazada circular
+    //Que significa que sea una Lista de Doblemente enlazadas circular?, 
+    //Significa que puede recorrer los valores desde el primer nodo hasta el ultimo y diseversa (doble) del ultimo hasta el primero de manera infinita.
     // 3, 5, 6, 7, 9, 1, 12, 34, 25, 30
-    internal class DoublyLinkedList
+    internal class CircularDoublyLinkedList 
     {
 
         //Variable de la cabeza (Primer valor de la lista)
         //private LDENode head = new LDENode();
-        internal LDENode head;
+        internal LDCNode head;
 
         //Variable ultima(Ultimo valor de la lista)
-        internal LDENode last;
+        internal LDCNode last;
 
         //Variable nodo
         internal int n;
@@ -71,126 +73,172 @@ namespace LinkList
             last = null;
         }
         //Lista Doblemente Enlazada ------Creacion de Nodo nuevo
-        internal DoublyLinkedList()
+        internal CircularDoublyLinkedList ()
         {
            // indicarle que el numero de la lista empieza en 0
             this.n = 1;
         }
 
+        //LCD - 3, 5, 6, 7, 9, 1, 12, 34, 25, 30
+        //Primero=null   Ultimo=null    nuevo
         //Insertamos nodos a una lista
         //Creamos una inserción del nuevo nodo de enfrente y al final concatenamos para que pueda leer toda la lista 
         internal void InsertFront(int new_data)
         {
             //Declaramos nuevo nodo
-            LDENode newNode = new LDENode(new_data);
+            LDCNode newNode = new LDCNode(new_data);
             //Nodo Ejemplo=3
             //Declaramos a la consola que me introduzca nuevo nodo
             Console.Write("\n Ingrese el dato del nuevo nodo: ");
             //Convertimos valor introducido a la consola a valor entero
             newNode.Data = int.Parse(Console.ReadLine());
             //Porque indicamos que la lista no a sido creada por lo tanto no contiene ningun valor
-            if(this.head==null)
+            if (this.head == null)
             {
                 // 3, 5, 6, 7, 9, 1, 12, 34, 25, 30
-                //Lista doblemente enlazada 
+                //Lista doblemente enlazada circular
                 //null <------3-------> null
 
                 //Indicamos que el primer nodo de la lista este nuevo
                 //Apuntador indicando que Primero=null y eso significa que va apuntar al nodo que le indiquemos en este caso es 3
                 //Primero=3
                 head = newNode;
-                //el nodo 3 contiene un apuntador a 3, es el siguiente dato que va imprimir o aparecer
-               //Apunta al siguiente
-                //3 apunta a null
-                head.Next = null;
-                //Apuntador hacia atras
-                //Contiene el apuntador hacia atras, indica que tampoco tiene nada por detras
-                head.Back = null;
-                //Indicar que tambien el ultimo puede ser el primero
-                //El ultimo siempre va hacer el primero
-                //Primero =3 ultimo=3
-                //el ultimo va hacer el primero osea 3
-                last = head;
+                //Como le indicamos que el primero nuevo nodo igualmente hacemos lo mismo con el ultimo nodo
+                last = newNode;
+                //Despues definir sus apuntadores 
+                //Primero=3 
+                //...3 --> 3
+                //Como ya sabemos que el nodo head es el primero le damos siguiente y posteriormenete se apunta a el mismo
+                head.Next = head;
+                //tmb contiene un apuntador que lo apunto hasta al ultimo
+                head.Back = last;
+                // ultimo <---- 3 ----> primero (siempre habra un circulo de ambos partes por eso es circular
+                //y andemas porque contiene doble apuntador de (siguiente-next) y (hacia atras-back)    
+                //Ya declaramos la cabeza de la lista
+
+
             }
             else
-            //El segundo nodo es 5
+
             {
-                // 3(el ultimo nodo) va apuntar al nuevo nodo sea 5
+
+                //ahora necesitamos que el ultimo apuntador sea el nuevo
+                // 3 --> siguiente --> 5 a donde apunta al nuevo nodo al siguiente
                 last.Next = newNode;
-                //tmb ese nuevo nodo tiene un apuntar a siguiente
-                //null <--- 3 --> 5---> null
-                newNode.Next = null;
-                //tmb contiene un apuntador hacia atras
-                //el nuevo nodo va apuntar al ultimo
-                //null <--- 3 --> <----5---> null
-                newNode.Back = last;
-                //Indicamos ahora que el ultimo es el nuevo nodo
-                // y cual es el nuevo nodo el 5
-                //null < ---3-- > < ----5---> null (6) Ahora el ultimo va hacer el nuevo nodo que metamos
-                //null < ---3-- > < ----5---> 6 --> null (igual tiene un apuntador a siguiente que es null)
-                //null < ---3-- > < ----5--->  <---6 --> null (igual tiene un apuntador que apunta al siguiente)
-                //y ahora el ultimo valor de nuestra lista es el nuevo nodo =6
-                //Primero=3 ultimo= ya no es 3 ahora es 5
+                //Indicamos que el nuevo nodo apunte al ultimo 
+                // 3 <---5
+                newNode.back = last;
+                //El nuevo nodo tmb tiene una puntador a siguiente y necesitamos que apunte al primero
+                //Indicamos que el apuntador tmb nos indique al primero osea
+                // 5 ---> 3
+                //Y asi forma un circulo
+                newNode.next = head;
+                //tmb el ultimo va a tener un apuntador al primero
+                //ultimo = 5 (porque es el ultimo nodo que hemos ingresado)
                 last = newNode;
+                //tmb necesitamos un apuntador, que si estamos en primero que nos dirija al ultimo
+                //que si el primero tiene un apuntador atras que nos dirija al ultimo
+                // 5 <------3 No apunte asi mismo si no al ultimo
+                head.back = last;
+                // 3, 5, 6, 7, 9, 1, 12, 34, 25, 30
+                //Genera una circunferencia
+                //Y contiene 2 apuntadores
+                //3<---- 3 ----><---- 5 ----><---- 6 -----><---- 7 ---->3 
                 n++;
             }
-            Console.Write("\n Nuevo nodo ingresado con exito \n"); 
+            Console.Write("\n Nuevo nodo ingresado con exito \n");
+            
             /*    
             newNode.next = this.head;
             this.head = newNode;
          */
 
         }
+         
 
-        //Desplegar lista doble
+        //Desplegar lista doble circular
         //Creamos un nuevo nodo que se llama actual (present)
         //Metodo para recorer desde el primero hasta el ultimo
+        //Metodo desplegar lista
         internal void displaylistPU(int new_present)
         {
             //Declaramos nuevo nodo
             //vamos desde el primer nodo recorriendo la lista
             //1. Creamos un nuevo apuntador llamado actual que hace referencia al primer nodo de la lista el 3
-            LDENode present = new LDENode(new_present);
-            present = head;
-            //mientras actual sea diferente de null, me recorre la lista
-            //2.Actual es diferente de 3 si, entonces imprimimos un dato 
-            while(present !=null)
-            {
-                //Los nodos datos que vayas encontrandolos imprimelos
-                Console.WriteLine(" " + present.Data);
-                //y el nodo recorre la lista con el apuntador Next
-                // y ahora el actual no es 3 si  no al siguiente al 5 y asi con todos los valores, eres diferente de null si entonces recorre la lista
-                present = present.Next;
-                // se termina la lista cuando al final no haiga mas valores que null
-            }
+            LDCNode present = new LDCNode(new_present);
 
+            //1.Aqui le estoy indicando que el valor actual sea igual al primero
+            //primero=3, actual=3
+            present = head;
+            //Actual es diferente a null si
+            if (present != null)
+            {
+                do
+                {
+                    //Imprimo actual dato
+                    //Si encuenta datos que me los imprima con esa condicion
+                    Console.WriteLine(" " + present.Data);
+                    //Imprimira el 3
+                    //Nodo apunta al siguiente 3 --->5
+                    present = present.next;
+
+
+                }//actual es diferente al primero, si porque son 3 y 5
+                while (present != head);
+            }
+            else
+            {
+                Console.WriteLine("\n La lista se encuenta vacia");
+            }
+            //3<- 3 ->, <- 5 ->,  6, 7, 9, 1, 12, 34, 25, 30
+          
         }
         //Desplegar la lista desde el ultimo hacia el primero
         internal void displaylistUP(int new_present)
         {
-            LDENode present = new LDENode(new_present);
+            //<-<-<-<-<-<-<-<-<-
+            LDCNode present = new LDCNode(new_present);
             //Empiece desde el ultimo
             present = last;
-            //Nos permite recorrer la lista mientras actual sea diferente de null
-            while(present!=null)
+
+            if(present !=null)
             {
-                //Mientras vaya recorriendo los nodos, que nos imprima que va encontrando
-                Console.WriteLine(" " + present.Data);
-                //usamos apuntar atras es el valor que esta destras de el en este caso recorre la lista hacia atras siendo ser 3
-                //ultimo hasta el primero
-                present = present.Back;
-                //hasta que termina la lista en null
+                //necesitamos recorerla por eso necesitamos el do, while
+                do
+                {
+                    Console.WriteLine(" " + present.data);
+                    //actual dato con apuntador hace atras
+                    present = present.Back;
+                }
+                while (present != last);
+            }
+            else
+            {
+                Console.WriteLine("\n La lista se encuentra vacia");
             }
         }
+
+        
+        //Nos permite recorrer la lista mientras actual sea diferente de null
+        /*   while(present!=null)
+           {
+               //Mientras vaya recorriendo los nodos, que nos imprima que va encontrando
+               Console.WriteLine(" " + present.Data);
+               //usamos apuntar atras es el valor que esta destras de el en este caso recorre la lista hacia atras siendo ser 3
+               //ultimo hasta el primero
+               present = present.Back;
+               //hasta que termina la lista en null
+           }
+       }*/
 
 
         //Obtener el ultimo nodo
         //Recurre lista a valor nulo
         //Aqui hacemos referencia para indicarle al nodo de la cabeza(el primero), que antes de el llevara null, para indicarle que antes de el no hay nada, le indicamos que desde ahí comienza la lista como tal
 
-        internal LDENode getLastNode()
+        internal LDCNode getLastNode()
         {
-            LDENode tmp = this.head;
+            LDCNode tmp = this.head;
             if (this.isEmpty())
                 return null;
             while (tmp.next != null)
@@ -204,7 +252,7 @@ namespace LinkList
         //Aqui hacemos la insercion del nodo con referencia a nulo. y al final concatenamos para que pueda leer toda la lista
         internal void InsertLast(int new_data)
         {
-            LDENode newNode = new LDENode(new_data);
+            LDCNode newNode = new LDCNode(new_data);
             //Aqui creamos un if para llamar a un metodo que hace referencia al valor nulo IsEmpty
             if (isEmpty())
             //if(head==null)
@@ -215,7 +263,7 @@ namespace LinkList
 
             }
 
-            LDENode lastNode = this.getLastNode();
+            LDCNode lastNode = this.getLastNode();
             lastNode.next = newNode;
 
             n++;
@@ -247,8 +295,8 @@ namespace LinkList
                 else
                 {
 
-                    LDENode prev = this.head;
-                    LDENode curr;
+                    LDCNode prev = this.head;
+                    LDCNode curr;
                     while (prev.next != null)
                     {
                         curr = prev.next;
@@ -266,7 +314,7 @@ namespace LinkList
         //Aqui imprimimos(Desplegamos) la lista de los datos de nuestra clase
         internal void printList()
         {
-            LDENode tmp = this.head;
+            LDCNode tmp = this.head;
             if (tmp != null)
                 Console.WriteLine(tmp.data);
             while (tmp.next != null)
@@ -286,7 +334,7 @@ namespace LinkList
     {
         static void Main(string[] args)
     {
-        DoublyLinkedList myList = new DoublyLinkedList();
+        CircularDoublyLinkedList  myList = new CircularDoublyLinkedList ();
 
         Random r = new Random();
         //int[] lala = { 3, 5, 6, 7, 9, 1, 12, 34, 25, 30 };
@@ -298,7 +346,7 @@ namespace LinkList
             }
 
         //Indicamos cabeza principal
-        LDENode tmp = myList.head;
+        LDCNode tmp = myList.head;
         if (tmp != null)
             Console.WriteLine(tmp.data);
         while (tmp.next != null)
